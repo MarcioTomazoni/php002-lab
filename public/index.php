bd382deaaf46b305b7d3c88d6d586fa456294f60
Marcio Tomazoni <marciotz13@gmail.com>
2026-04-17 06:26:10 -0300

    feat: implementa roteamento básico no front controller

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
 
-echo "Front Controller funcionando!";
+require_once __DIR__ . '/../controllers/AuthController.php';
 
+// Simples roteamento via GET
+$action = $_GET['action'] ?? 'login';
+
+$controller = new AuthController();
+
+if ($action === 'login') {
+    $controller->login();
+} else {
+    echo "Rota não encontrada";
+}
