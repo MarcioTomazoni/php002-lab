d660830036583d43de40e73d22d012831b6a2237
Marcio Tomazoni <marciotz13@gmail.com>
2026-04-23 08:34:53 -0300

    feat: adiciona views de login e dashboard no MVC

---
+++ views/auth/dashboard.php
@@ -0,0 +1,5 @@
+<h2>Dashboard</h2>
+
+<p>Bem-vindo, <?php echo $_SESSION['user_nome']; ?>!</p>
+
+<a href="index.php?action=login">Sair</a>
--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,13 @@
-<h2>Tela de Login (view)</h2>
+<h2>Login</h2>
+
+<?php if (!empty($erro)): ?>
+<p style="color:red;"><?php echo $erro; ?></p>
+<?php endif; ?>
+
+<form method="POST">
+    <input type="email" name="email" placeholder="E-mail" required><br><br>
+
+    <input type="password" name="senha" placeholder="Senha" required><br><br>
+
+    <button type="submit">Entrar</button>
+</form>
